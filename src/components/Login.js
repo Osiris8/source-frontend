d3dd738f5a46e7ee5470b84c732d78085b692c2f
Osiris8 <miganosiris8@gmaaail.com>
2023-08-26 19:24:29 +0200

    add login component

---
+++ src/components/Login.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "../styles/start.css";
+function Login() {
+  return (
+    <div class="w-full h-full h-screen flex justify-center items-center background-image bg-cyan-500 hover:bg-cyan-600 ">
+      <form class="bg-white shadow-md rounded px-14 pt-6 pb-8 mb-4 ">
+        <h2 className="text-2xl mb-4 font-extrabold text-center text-cyan-900">
+          Source
+        </h2>
+
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="username"
+          >
+            Email
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Enter your username"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="password"
+          >
+            Password
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign In
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-cyan-500 hover:text-cyan-800"
+            href="https://hello.com"
+          >
+            Forgot Password?
+          </a>
+        </div>
+        <div className="flex items-center justify-center m-5">
+          <a
+            className="inline-block align-baseline font-bold text-sm text-cyan-500 hover:text-cyan-800"
+            href="https://hello.com"
+          >
+            S'inscrire
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
