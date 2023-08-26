ce34bcb078319fd38b87137f16211b709835a2c9
Osiris8 <miganosiris8@gmaaail.com>
2023-08-26 22:51:41 +0200

    delete Resgister component in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 //import logo from './logo.svg';
 //import './App.css';
 
-import Register from "./components/Register";
+//import Register from "./components/Register";
 import Login from "./components/Login";
 
 function App() {
   return (
     <div>
-      <Register />
       <Login />
     </div>
   );
