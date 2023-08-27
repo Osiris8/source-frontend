3d82836bc39dd821dda788f6b96e080d0ef46f2f
Osiris8 <miganosiris8@gmaaail.com>
2023-08-28 00:44:01 +0200

    configure Route of Application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-//import logo from './logo.svg';
-//import './App.css';
-
-//import Register from "./components/Register";
-import Login from "./components/Login";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import Login from "./components/Login"; // Importez votre composant de connexion
+import Home from "./components/Home";
+import Register from "./components/Register";
+import { Navigate } from "react-router-dom";
+import { UidContext } from "./components/AppContext";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Fonction pour vérifier l'authentification de l'utilisateur
+    const checkAuthentication = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL}api/user/verify`,
+          {
+            withCredentials: true,
+          }
+        );
+
+        if (response.data.user) {
+          setUser(response.data.user._id);
+        }
+      } catch (error) {
+        console.log(error);
+        console.log(
+          "Une erreur est survenue lors de la vérification de l'authentification."
+        );
+      }
+    };
+
+    checkAuthentication();
+  }, [user]);
+
   return (
-    <div>
-      <Login />
-    </div>
+    <UidContext.Provider value={user}>
+      <Router>
+        <Routes>
+          <Route path="/" element={user ? <Home /> : <Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/home"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </UidContext.Provider>
   );
 }
 
