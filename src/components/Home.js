304e8045643de6f7b9f63db931cda83e3537abd3
Osiris8 <miganosiris8@gmaaail.com>
2023-08-29 23:57:57 +0200

    add Navbar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 export default Home;
