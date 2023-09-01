5b96c74cf800e40521d9c9f257dc469babe2c2be
Osiris8 <miganosiris8@gmaaail.com>
2023-09-01 22:31:32 +0200

    add upload_picture type for redux

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,5 @@
 // types.js
 export const GET_USER = "GET_USER"; // Définissez votre type d'action
 export const GET_USER_PROFILE = "GET_USER_PROFILE";
+// Action type
+export const UPLOAD_PICTURE = "UPLOAD_PICTURE";
