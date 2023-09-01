3422a656a9d852be89e178aead2ca00c03ece6d8
Osiris8 <miganosiris8@gmaaail.com>
2023-09-01 22:35:28 +0200

    add uploadPicture action

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 // userActions.js
 import { GET_USER } from "./types"; // Importez le type d'action
 import { GET_USER_PROFILE } from "./types";
+import { UPLOAD_PICTURE } from "./types";
 
 // Action creator pour mettre à jour l'état de l'utilisateur
 export const getUser = (userId) => {
@@ -22,6 +23,30 @@ export const getUserProfile = (userId) => {
       })
       .catch((err) => {
         console.log(err);
+      });
+  };
+};
+
+// Action creator
+export const uploadPicture = (data, userId) => {
+  return (dispatch) => {
+    return axios
+      .put(
+        `${process.env.REACT_APP_BASE_URL}api/user/uploadProfile/${userId}`,
+        data,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true, // Ajout de cette ligne
+        }
+      )
+      .then((res) => {
+        dispatch({ type: UPLOAD_PICTURE, payload: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("ça ne passe pas");
       });
   };
 };
