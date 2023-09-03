4b5cd644b8e56a5ba838c1dbabef64221b20693e
Osiris8 <miganosiris8@gmaaail.com>
2023-09-03 21:55:32 +0200

    update store.js file

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../reducers/userReducer";
+import usersReducer from "../reducers/usersReducer";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    users: usersReducer,
     // Autres reducers si vous en avez
   },
 });
