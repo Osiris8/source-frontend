7eefe0b7c7ab10a23edb8dd73d23836b520dadb1
Osiris8 <miganosiris8@gmaaail.com>
2023-09-03 21:52:09 +0200

    add usersReducer in store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
+import usersReducer from "./usersReducer";
 export default combineReducers({
   userReducer,
+  usersReducer,
 });
