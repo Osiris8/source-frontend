04f4d41c1947bea6748bd78e420ebb21a07ea659
Osiris8 <miganosiris8@gmaaail.com>
2023-09-04 22:01:39 +0200

    update types.js

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,4 @@
 // types.js
-export const GET_USER = "GET_USER"; // Définissez votre type d'action
 export const GET_USER_PROFILE = "GET_USER_PROFILE";
 // Action type
 export const UPLOAD_PICTURE = "UPLOAD_PICTURE";
