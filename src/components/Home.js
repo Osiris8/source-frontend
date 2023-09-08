6af881d86f8a4524b20e1cff6ea12048796e086b
Osiris8 <miganosiris8@gmaaail.com>
2023-09-08 14:02:19 +0200

    add card component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllPosts } from "../actions/postActions";
 import Navbar from "./Navbar";
+import Card from "./Card";
 function Home() {
-  return <Navbar />;
+  const dispatch = useDispatch();
+  const [loadPost, setLoadPost] = useState(true);
+  const usersProfiles = useSelector((state) => state.users.allUsers);
+  const userProfile = useSelector((state) => state.user.userProfile);
+  const posts = useSelector((state) => state.posts.allPosts);
+
+  useEffect(() => {
+    if (loadPost) {
+      dispatch(getAllPosts());
+      setLoadPost(false);
+    }
+  }, [dispatch, loadPost]);
+
+  return (
+    <div>
+      <Navbar />
+      <div>
+        {posts.map((post) => (
+          <Card post={post} key={post._id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Home;
