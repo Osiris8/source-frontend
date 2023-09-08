284de27ffb28485efca656d315171551e49e9ee6
Osiris8 <miganosiris8@gmaaail.com>
2023-09-08 14:07:45 +0200

    update store.js - add postReducer in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../reducers/userReducer";
 import usersReducer from "../reducers/usersReducer";
-
+import postReducer from "../reducers/postReducer";
 export default configureStore({
   reducer: {
     user: userReducer,
     users: usersReducer,
+    posts: postReducer,
     // Autres reducers si vous en avez
   },
 });
