0947d239d4370c67cb9dc5e90e1c489034cdfd55
Osiris8 <miganosiris8@gmaaail.com>
2023-09-11 11:14:26 +0200

    update postReducer and add LIKE_POST case and UNLIKE_POST case

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,5 @@
 import { GET_ALL_POSTS } from "../actions/types";
+import { LIKE_POST, UNLIKE_POST } from "../actions/types";
 
 const initialState = {
   allPosts: [], // Initialiser le tableau des posts comme vide
@@ -10,6 +11,40 @@ const postReducer = (state = initialState, action) => {
       return {
         ...state,
         allPosts: action.payload, // Mettez à jour le tableau des posts avec les données reçues
+      };
+    case LIKE_POST:
+      const likedPostId = action.payload.postId;
+      // Créez une nouvelle copie de allPosts avec les likers mis à jour
+      const updatedAllPosts = state.allPosts.map((post) => {
+        if (post._id === likedPostId) {
+          // Créez une nouvelle copie du post avec les likers mis à jour
+          return {
+            ...post,
+            likers: [...post.likers, action.payload.userId],
+          };
+        }
+        return post;
+      });
+      return {
+        ...state,
+        allPosts: updatedAllPosts,
+      };
+    case UNLIKE_POST:
+      const unlikedPostId = action.payload.postId;
+      // Créez une nouvelle copie de allPosts avec les likers mis à jour
+      const updatedUnlikedPosts = state.allPosts.map((post) => {
+        if (post._id === unlikedPostId) {
+          // Créez une nouvelle copie du post avec les likers mis à jour
+          return {
+            ...post,
+            likers: post.likers.filter((id) => id !== action.payload.userId),
+          };
+        }
+        return post;
+      });
+      return {
+        ...state,
+        allPosts: updatedUnlikedPosts,
       };
     default:
       return state;
