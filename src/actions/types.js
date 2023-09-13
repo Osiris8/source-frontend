59273f9eac529d4628b377682abc9ec410a82b7e
Osiris8 <miganosiris8@gmaaail.com>
2023-09-13 09:54:07 +0200

    add DELETE_POST type

--- src/actions/types.js
+++ src/actions/types.js
@@ -9,3 +9,4 @@ export const GET_ALL_POSTS = "GET_ALL_POSTS";
 export const LIKE_POST = "LIKE_POST";
 export const UNLIKE_POST = "UNLIKE_POST";
 export const UPDATE_POST = "UPDATE_POST";
+export const DELETE_POST = "DELETE_POST";
