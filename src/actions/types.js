83fd93205d8b38e0c1ad4fedec64f07e88dbda92
Osiris8 <miganosiris8@gmaaail.com>
2023-09-16 00:42:49 +0200

    add EDIT_COMMENT and DELETE_COMMENT types

--- src/actions/types.js
+++ src/actions/types.js
@@ -11,3 +11,5 @@ export const UNLIKE_POST = "UNLIKE_POST";
 export const UPDATE_POST = "UPDATE_POST";
 export const DELETE_POST = "DELETE_POST";
 export const ADD_COMMENT = "ADD_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
