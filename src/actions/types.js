70fc78f3ac7318e94196c4f3a22499bbece41b59
Osiris8 <miganosiris8@gmaaail.com>
2023-09-15 21:38:34 +0200

    add ADD_COMMENT type

--- src/actions/types.js
+++ src/actions/types.js
@@ -10,3 +10,4 @@ export const LIKE_POST = "LIKE_POST";
 export const UNLIKE_POST = "UNLIKE_POST";
 export const UPDATE_POST = "UPDATE_POST";
 export const DELETE_POST = "DELETE_POST";
+export const ADD_COMMENT = "ADD_COMMENT";
