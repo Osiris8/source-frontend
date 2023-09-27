88a54ac27a0e64ce63b9b36c1bdbe3297615dba5
Osiris8 <miganosiris8@gmaaail.com>
2023-09-27 12:10:08 +0200

    update formatDateToReadableDate function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,11 @@
 export default function formatDateToReadableDate(dateString) {
-  const options = { year: "numeric", month: "long", day: "numeric" };
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
   const formattedDate = new Date(dateString).toLocaleDateString(
     undefined,
     options
